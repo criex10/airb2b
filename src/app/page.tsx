f173c57bd94ecbd532e58f5341bbfea3107ff465
criex10 <criex10@gmail.com>
2023-12-03 16:37:08 -0800

    Add in static header and hero components
    Adds in the resources for the static content on the page.
    Also put in a placeholder for where the dynamic content will
    go.
    
    https://scrimba.com/learn/learnreact/project-setup-hero-co90845d2aecdbb1a1c84a3ad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
+import Image from "next/image";
+import logo from '../images/airbnb-logo.png';
+import hero from '../images/experiences.png';
 
+function Header() {
+    return (
+        <nav className={styles.nav}>
+            <Image
+                src={logo}
+                alt={'Airbnb logo'} />
+        </nav>
+    );
+}
+
+function Hero() {
+    return (
+        <div className={styles.hero}>
+            <Image src={hero}
+                   alt={'Examples of online experiences we offer'} />
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    );
+}
+
+function Carousel() {
+    return (
+        <div className={styles.carousel}>
+            <ul>
+                <li>Swimming placeholder</li>
+                <li>Wedding placeholder</li>
+                <li>Biking placeholder</li>
+            </ul>
+        </div>
+    )
+}
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Hello, world!</h1>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <Header />
+            <Hero />
+            <Carousel />
+        </main>
+    );
 }
