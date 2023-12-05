955aba42a5557f9052db090112f63b511a4ca670
criex10 <criex10@gmail.com>
2023-12-04 17:17:07 -0800

    Add prototype Card design
    This adds in a basic card that is templatized for the inevitable
    refactoring to make it a modular component.
    
    The CSS is probably wrong; only the presentation looks right.
    
    https://scrimba.com/learn/learnreact/project-card-component-co1954a349a2aaba7383a6cbe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import styles from './page.module.css';
-import Image from "next/image";
+import Image, {StaticImageData} from "next/image";
 import logo from '../images/airbnb-logo.png';
 import hero from '../images/experiences.png';
+import package1Image from '../images/package-1.png'
+import star from '../images/star.svg'
 
 function Header() {
     return (
@@ -24,11 +26,43 @@ function Hero() {
     );
 }
 
+function Card() {
+    let title: string = 'Life lessons with Katie Zaferes';
+    let rating: number = 5;
+    let numRatings: number = 6;
+    let country: string = 'USA';
+    let priceInUsd: string = '136';
+    let units: string = 'person';
+    let inventoryStatus: string = 'SOLD OUT';
+    let packageImage: StaticImageData = package1Image;
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.card_inventoryStatus}>{inventoryStatus}</div>
+            <Image src={packageImage}
+                   alt={'Package image'}
+                   className={styles.card_packageImage} />
+
+            <span className={styles.rating}>
+                <Image src={star} alt={'Star'} className={styles.rating_star}/>
+                <span className={styles.rating_number}> {rating.toFixed(1)} </span>
+                {
+                    `(${numRatings}) 
+                    \u2022
+                    ${country}`
+                }
+            </span>
+            <p className={styles.card_title}>{title}</p>
+            <p className={styles.card_price}><b>From ${priceInUsd}</b> / {units}</p>
+        </div>
+    )
+}
+
 function Carousel() {
     return (
         <div className={styles.carousel}>
             <ul>
-                <li>Swimming placeholder</li>
+                <li><Card /></li>
                 <li>Wedding placeholder</li>
                 <li>Biking placeholder</li>
             </ul>
